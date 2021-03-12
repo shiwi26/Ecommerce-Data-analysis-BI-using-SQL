67c168ef14bd8418b0cefe306d4c69e1fffafaaa
shiwi26 <64558669+shiwi26@users.noreply.github.com>
2021-03-11 16:20:21 -0800

    Delete Analyzing Traffic.sql

--- Analyzing Traffic.sql
+++
@@ -1,7 +0,0 @@
-USE mavenfuzzyfactory;
-select utm_source, utm_campaign, http_referer, 
-count(distinct website_session_id) as sessions
-from website_sessions
-where created_at < '2012-04-12'
-group by 1,2,3
-order by sessions desc
